e321dee1cbfade884ead1f88441dff0a87b1897c
João Carlos Rebouças <70778525+jcr04@users.noreply.github.com>
2025-06-05 20:25:24 -0300

    add

--- lib/models/product.dart
+++ lib/models/product.dart
@@ -1,15 +1,25 @@
 class Product {
   final String id;
   final String name;
-  final String imageUrl;
   final String description;
   final double price;
+  final String imageUrl;
 
   Product({
     required this.id,
     required this.name,
-    required this.imageUrl,
     required this.description,
     required this.price,
+    required this.imageUrl,
   });
+
+  factory Product.fromJson(Map<String, dynamic> json) {
+    return Product(
+      id: json['id'],
+      name: json['name'],
+      description: json['description'],
+      price: (json['price'] as num).toDouble(),
+      imageUrl: json['imageUrl'],
+    );
+  }
 }
